6e58ff3f8c68a5eac65773b92a7f988829041c49
Tom MacWright <tom@macwright.org>
2016-08-22 13:23:48 -0400

    Fix identation

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var MapboxClient = require('mapbox');
-
-var request = require('supertest');
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
+
+var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.json());
@@ -11,19 +10,18 @@ app.use(bodyParser.json());
 var MapboxClient = require('mapbox');
 var client = new MapboxClient('sk.eyJ1IjoiZ3JhZmEiLCJhIjoiY2lyeTVtcXhxMDFzYjJva2pwZDg2cDMzbSJ9.4r8RuCe2Bv1tHSHbZwDUlQ');
 
-
 app.get('/', function (req, res) {
   client.listFeatures('ciql88hm60004fpnhfytkz0v6', {}, function(err, collection) {
-	  if (err) return res.status(500);
-	  res.json(collection);
-	});
+    if (err) return res.status(500);
+    res.json(collection);
+  });
 });
 
 app.post('/', function (req, res) {
-	client.insertFeature(req.body, 'ciql88hm60004fpnhfytkz0v6', function(err, feature) {
-	  if (err) return res.status(500);
-	  res.json(feature);
-	});
+  client.insertFeature(req.body, 'ciql88hm60004fpnhfytkz0v6', function(err, feature) {
+    if (err) return res.status(500);
+    res.json(feature);
+  });
 });
 
 app.listen(app.get('port'), function() {
