e02ff9a0ade14f310636c77fa090384b6519904d
Tom MacWright <tom@macwright.org>
2016-08-22 13:46:18 -0400

    Implement remainder of methods

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # datasets-api-gateway
-An example server application for handling requests to the Datasets API
+
+An example server application for handling requests to the Mapbox Datasets API.
+
+## Running
+
+Required environment variables:
+
+* `MapboxAccessToken`: an access token with datasets:read and datasets:write
+  scopes.
+* `MapboxDatasetID`: the ID of the wrapped dataset.
--- index.js
+++ index.js
@@ -1,31 +1,55 @@
 var MapboxClient = require('mapbox');
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var MapboxClient = require('mapbox');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.json());
 
-var MapboxClient = require('mapbox');
-var client = new MapboxClient('sk.eyJ1IjoiZ3JhZmEiLCJhIjoiY2lyeTVtcXhxMDFzYjJva2pwZDg2cDMzbSJ9.4r8RuCe2Bv1tHSHbZwDUlQ');
+var client = new MapboxClient(process.env.MapboxAccessToken);
+if (!process.env.MapboxDatasetID) throw new Error('MapboxDatasetID environment variable required');
 
-app.get('/', function (req, res) {
-  client.listFeatures('ciql88hm60004fpnhfytkz0v6', {}, function(err, collection) {
+app.get('/features', function (req, res) {
+  client.listFeatures(process.env.MapboxDatasetID, {}, function(err, collection) {
+
     if (err) return res.status(500);
     res.json(collection);
+
   });
 });
 
-app.post('/', function (req, res) {
-  client.insertFeature(req.body, 'ciql88hm60004fpnhfytkz0v6', function(err, feature) {
+app.get('/features/:featureID', function (req, res) {
+  client.readFeature(req.param.featureID, process.env.MapboxDatasetID, function(err, collection) {
+
     if (err) return res.status(500);
-    res.json(feature);
+    res.json(collection);
+
+  });
+});
+
+app.delete('/features/:featureID', function (req, res) {
+  client.deleteFeature(req.param.featureID, process.env.MapboxDatasetID, function(err, collection) {
+
+    if (err) return res.status(500);
+    res.json(collection);
+
+  });
+});
+
+app.put('/features/:featureID', function (req, res) {
+  client.insertFeature(req.body, process.env.MapboxDatasetID, function(err, collection) {
+
+    if (err) return res.status(500);
+    res.json(collection);
+
   });
 });
 
 app.listen(app.get('port'), function() {
+
   console.log('Node app is running on port', app.get('port'));
+
 });
 
 module.exports = app;
